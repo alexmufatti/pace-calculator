5a0201ac871bcf1f425b8bb6b6c36c5f3f489178
Alex Mufatti <me@alexmufatti.it>
2018-12-14 18:07:14 +0100

    first version

---
+++ src/timeUtils.ts
@@ -0,0 +1,58 @@
+class TimeUtils {
+
+
+        getTotalSeconds(timeString: string) {
+            const t = this.getSecMinHours(timeString);
+            if (isNaN(t.sec) || isNaN(t.sec) ||isNaN(t.sec))
+                return 0;
+            return (t.sec) + (t.min*60) + (t.hou*60*60);
+        }
+
+        getSeconds(timeString: string) {
+            if (timeString.split(':').length - 1 < 0) return 0;
+            if (timeString.split(':')[timeString.split(':').length - 1] === '') return 0;
+            return parseInt(timeString.split(':')[timeString.split(':').length - 1]);
+        }
+
+        getMinutes(timeString) {
+            if (timeString.split(':').length - 2 < 0) return 0;
+            if (timeString.split(':')[timeString.split(':').length - 2] === '') return 0;
+            return parseInt(timeString.split(':')[timeString.split(':').length - 2]);
+        }
+
+        getHours(timeString) {
+            if (timeString.split(':').length - 3 < 0) return 0;
+            if (timeString.split(':')[timeString.split(':').length - 3] === '') return 0;
+            return parseInt(timeString.split(':')[timeString.split(':').length - 3]);
+        }
+
+        getSecMinHours(timeString) {
+            return {
+                sec: this.getSeconds(timeString),
+                min: this.getMinutes(timeString),
+                hou: this.getHours(timeString)
+            };
+        }
+
+        pad(value, char) {
+            value = value + '';
+            return char.substring(0, char.length - value.length) + value;
+        }
+
+        createTimeString(seconds) {
+            var sec = this.pad(Math.round(seconds % 60), '00');
+            var hours = this.pad(Math.floor(seconds / 3600), '0');
+            var min = this.pad(Math.floor((seconds - (hours * 3600)) / 60), '00');
+
+            return hours + ':' + min + ':' + sec;
+        }
+        createPaceString(seconds) {
+            var sec = this.pad(Math.round(seconds % 60), '00');
+            var min = this.pad(Math.floor(seconds / 60), '00');
+
+            return min + ':' + sec;
+        }
+
+    }
+
+export default new TimeUtils();
