0a6e632286e0d3610096b29d7a93399ea9ec7654
Alex Mufatti <me@alexmufatti.it>
2024-04-24 17:02:29 +0200

    update

--- src/Models/Lap.ts
+++ src/Models/Lap.ts
@@ -1,31 +1,27 @@
 import Time from "./Time";
 
 class Lap {
-  constructor(index: number, pace: string, totalDistance: number, startTime: string) {
-    this.index = index;
+  constructor(private index: number, private pace: number, private totalDistance: number, private startTime: string ) {
     this.left =
-      totalDistance - (this.index + 1) >= 1 ? this.index + 1 : totalDistance;
-    this.time = Time.getTime(this.left * Time.getTotalSeconds(pace));
+      this.totalDistance - (this.index + 1) >= 1 ? this.index + 1 : this.totalDistance;
+    this.time = Time.getTime(this.left * this.pace);
 
-    this.totalDistance = totalDistance;
     this.key = this.label();
 
-    const time = new Date().setHours(parseInt(startTime.split(':')[0]), parseInt(startTime.split(':')[1]));
+    const time = new Date().setHours(parseInt(this.startTime.split(':')[0]), parseInt(this.startTime.split(':')[1]));
     const lapTime = new Date(time + this.time.hou * 60 * 60 * 1000
         + this.time.min * 60 * 1000
         + this.time.sec * 1000)
     this.passTime = `${ Time.pad(lapTime.getHours().toString(),'00')}:${ Time.pad(lapTime.getMinutes().toString(),'00')}`
   }
 
-  totalDistance: number;
-  index: number;
   time: Time;
   left: number;
   key: string;
   passTime: string;
 
   label() {
-    return "Km " + this.left;
+    return (Math.round(this.left*100)/100).toString();
   }
 }
 
--- src/components/Laps.tsx
+++ src/components/Laps.tsx
@@ -1,8 +1,11 @@
 import Lap from "../Models/Lap.ts";
 import {useRef, useState} from "react";
-import {ActionIcon, Button, rem, Stack, Table, Title} from "@mantine/core";
+import {ActionIcon, Button, Flex, rem, Stack, Table, Title} from "@mantine/core";
 import {TimeInput} from "@mantine/dates";
 import {IconClock} from "@tabler/icons-react";
+import Time from "../Models/Time.ts";
+
+const MileToMk = 1.60934;
 
 export default function Laps(props: { distance: string, pace: string }) {
     const ref = useRef<HTMLInputElement>(null);
@@ -10,14 +13,22 @@ export default function Laps(props: { distance: string, pace: string }) {
     const [startTime, setStartTime] = useState<string>(`${now.getHours()}:${now.getMinutes()}`);
     const [showLaps, setShowLaps] = useState<boolean>(false);
 
-    const calcLaps = (distance: string, pace: string): Array<Lap> => {
+    const calcLaps = (distance: string, pace: number): Array<Lap> => {
         const distanceF = parseFloat(distance);
         if (isNaN(distanceF) || distanceF <= 0) return [];
         return [...Array(Math.floor(distanceF))].map((_, idx) => {
             return new Lap(idx, pace, distanceF, startTime);
         });
     };
-    const laps = calcLaps(props.distance, props.pace);
+    const calcLapsMiles = (distance: string, pace: number): Array<Lap> => {
+        const distanceF = parseFloat(distance);
+        if (isNaN(distanceF) || distanceF <= 0) return [];
+        return [...Array(Math.floor(distanceF))].map((_, idx) => {
+            return new Lap(idx, pace, distanceF, startTime);
+        });
+    };
+    const laps = calcLaps(props.distance, Time.getTotalSeconds(props.pace));
+    const lapsMile = calcLapsMiles((parseFloat(props.distance) / MileToMk).toString(), Time.getTotalSeconds(props.pace) * MileToMk);
     const pickerControl = (
         <ActionIcon variant="subtle" color="gray" onClick={() => ref.current?.showPicker()}>
             <IconClock style={{width: rem(16), height: rem(16)}} stroke={1.5}/>
@@ -41,6 +52,7 @@ export default function Laps(props: { distance: string, pace: string }) {
                 >
                     Calculate Laps
                 </Button>
+                <Flex direction={'row'} gap={20}>
                 <Table hidden={!showLaps} horizontalSpacing="md" verticalSpacing="sm" striped withTableBorder
                        withColumnBorders>
                     <Table.Thead>
@@ -53,13 +65,33 @@ export default function Laps(props: { distance: string, pace: string }) {
                     <Table.Tbody>
                         {laps.map((l) => {
                             return (<Table.Tr>
-                                <Table.Td>{l.key}</Table.Td>
+                                <Table.Td>Km {l.key}</Table.Td>
                                 <Table.Td>{l.time.timeString()}</Table.Td>
                                 <Table.Td>{l.passTime}</Table.Td>
                             </Table.Tr>)
                         })}
                     </Table.Tbody>
                 </Table>
+                <Table hidden={!showLaps} horizontalSpacing="md" verticalSpacing="sm" striped withTableBorder
+                       withColumnBorders>
+                    <Table.Thead>
+                        <Table.Tr>
+                            <Table.Th>Lap</Table.Th>
+                            <Table.Th>Elapsed Time</Table.Th>
+                            <Table.Th>Time</Table.Th>
+                        </Table.Tr>
+                    </Table.Thead>
+                    <Table.Tbody>
+                        {lapsMile.map((l) => {
+                            return (<Table.Tr>
+                                <Table.Td>Mile {l.key}</Table.Td>
+                                <Table.Td>{l.time.timeString()}</Table.Td>
+                                <Table.Td>{l.passTime}</Table.Td>
+                            </Table.Tr>)
+                        })}
+                    </Table.Tbody>
+                </Table>
+                </Flex>
             </Stack>
         </Stack>);
 }
